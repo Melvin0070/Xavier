e4482c2feba02782dc1df11d7238746f39607bd0
Melvin Kannan <139456150+Melvin0070@users.noreply.github.com>
2025-11-20 21:44:40 +0530

    Change redirect URL for initialRedirectBtn

--- msf-new6.js
+++ msf-new6.js
@@ -231,7 +231,7 @@ class MultiStepFormV4 {
 
         if (this.elements.initialRedirectBtn) {
             this.elements.initialRedirectBtn.addEventListener('click', () => {
-                window.location.href = '/register?redirect=aistrategicplan';
+                window.location.href = '/sign-up-ai-strategic-plan';
             });
         }
         
